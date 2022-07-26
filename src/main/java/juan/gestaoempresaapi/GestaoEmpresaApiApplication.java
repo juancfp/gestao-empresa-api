9a6517470e591f163eaffe22dfe25011058c5208
Juan Carlos <juancfp.comercial@gmail.com>
2022-07-26 19:48:45 -0300

    commit inicial

---
+++ src/main/java/juan/gestaoempresaapi/GestaoEmpresaApiApplication.java
@@ -0,0 +1,13 @@
+package juan.gestaoempresaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEmpresaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEmpresaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/juan/gestaoempresaapi/GestaoEmpresaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package juan.gestaoempresaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEmpresaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
